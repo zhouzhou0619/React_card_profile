228500471c4358d222bd3f9bc89c3582e1955b51
Yihan Zhou <yzho0181@student.monash.edu>
2023-03-16 14:10:59 +1100

    reactapp

---
+++ src/ProfileCard.js
@@ -0,0 +1,27 @@
+function ProfileCard({title, handle, image, description}) {
+    // console.log(title, handle)
+    // const title = props.title
+    // const handle = props.handle
+    // const {title, handle} = props;
+
+    return ( 
+        <div className = "card">
+            <div className = "card-image">
+                <figure className = "image is-1by1">
+                  <img src = {image} alt = 'pda logo'/>
+                </figure>
+            </div>
+            <div className = "card-content">
+              <div className = "media-content">
+                <p className = "title is-4">{title}</p>
+                <p className = "subtitle is-6">{handle}</p> 
+              </div>
+              <div className = "content">{description}</div>
+            </div>
+            
+             
+        </div>
+    );
+}
+
+export default ProfileCard;
